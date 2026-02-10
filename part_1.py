e7bfe7d3e447d38df9e355bc5dc91d128007f5f3
Eh576 <Eh576@kent.ac.uk>
2026-02-10 12:33:18 +0000

    W6 P1

--- part_1.py
+++ part_1.py
@@ -1,1 +1,14 @@
+sample_bay = ["Basalt", "Silica", "Iron", "Dust"]
+print(sample_bay[0])
+print(sample_bay[-1])
+print(len(sample_bay))
 
+for sample in sample_bay:
+    print(f"Transmitting data for: {sample}")
+
+
+new_findings = []
+for _ in range(3):
+    rock = input("Enter a rock type : ")
+    new_findings.append(rock)
+print(new_findings) 
