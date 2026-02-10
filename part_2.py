a14f87fbd7e17b46fb8e9293ae58f7b682df3f2a
Eh576 <Eh576@kent.ac.uk>
2026-02-10 12:43:01 +0000

    W6 P2

--- part_2.py
+++ part_2.py
@@ -1,1 +1,25 @@
+rover_status = {
+    "Battery" : 100, 
+    "Heater" : "Off", 
+    "Camera" : "Standby"
+}
+print(rover_status["Battery"])
 
+rover_status["Battery"] = 85
+rover_status["Heater"] = "On"
+rover_status["Speed"] = 5
+print(rover_status) 
+
+mission_log = [
+    {
+        "Site": "Crater A", 
+        "Radiation": "Low", 
+        "Water": False
+    }, 
+    {
+        "Site": "Dune B", 
+        "Radiation": "High", 
+        "Water": True
+    }
+]
+print(mission_log) 
